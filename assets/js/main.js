495dbbb3f3e51468669f32acbead2f6250057713
Naiem-Devs <dnsnaiem@gmail.com>
2024-11-07 19:09:26 +0600

    done

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+(function($) {
+  "use strict";
+  
+
+
+  // owlCarousel
+  $(".brand-active").owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 6,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2
+      },
+      767: {
+        items: 3
+      },
+      992: {
+        items: 6
+      }
+    }
+  });
+
+
+  // page Animation
+  // AOS.init({
+  //   mirror: true,
+  //   duration: 1500,
+  //   initClassName: 'aos-init',
+  //   once: true,
+  // });
+
+  // data-aos="fade-up" 
+  // data-aos-delay="300" 
+
+ 
+})(jQuery);
